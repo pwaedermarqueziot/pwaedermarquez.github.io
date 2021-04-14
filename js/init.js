081d3cd006f0d31499dbaeaa0783bbc4dcf25e2a
pwaedermarqueziot <82473263+pwaedermarqueziot@users.noreply.github.com>
2021-04-14 00:17:09 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,11 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAm2OxLqNV2h69-z6NnLhDonRODqFNpA0A",
+    authDomain: "pwaedermarquez.firebaseapp.com",
+    projectId: "pwaedermarquez",
+    storageBucket: "pwaedermarquez.appspot.com",
+    messagingSenderId: "47048959279",
+    appId: "1:47048959279:web:dd568d9a94af7d43deedda"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
